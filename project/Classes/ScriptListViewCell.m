2bb6e6cde87feb1b8282e8b70925245cf5bd1abf
Josef Vanek <josef.vanek@intellicore.net>
2012-07-03 10:22:53 +0200

    Copyright change

--- project/Classes/ScriptListViewCell.m
+++ project/Classes/ScriptListViewCell.m
@@ -3,7 +3,7 @@
 //  iphone-irb
 //
 //  Created by Vaněk Josef on 29/06/12.
-//  Copyright (c) 2012 Intellicore. All rights reserved.
+//  Copyright (c) 2012 Pallas Software. All rights reserved.
 //
 
 #import "ScriptListViewCell.h"
